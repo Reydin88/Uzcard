9e02e26eab3aa3f6f1a7ce9e72d5e6cffccf01e4
Reydin88 <reydin888@gmail.com>
2025-05-27 02:10:57 +0500

    Update web.py

--- web.py
+++ web.py
@@ -1,1 +1,10 @@
-# Flask-панель: админка, WebApp, Excel, фильтры, статистика и прочее
+from flask import Flask
+
+app = Flask(__name__)  # ← это обязательно
+
+@app.route("/")
+def home():
+    return "uzcardsbot работает!"
+
+if __name__ == "__main__":
+    app.run()
