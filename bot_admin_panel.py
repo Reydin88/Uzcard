27c05f3d33c34d5d21217eb29a3373abacb46818
Reydin88 <reydin888@gmail.com>
2025-05-27 06:25:08 +0500

    Update bot_admin_panel.py

--- bot_admin_panel.py
+++ bot_admin_panel.py
@@ -4,7 +4,7 @@ import sqlite3, os
 from datetime import datetime
 import pandas as pd
 
-BOT_TOKEN = "8077158709:AAEvzGF3H4Ey1V597aHJcylpWirI0eXyGlQ"
+BOT_TOKEN = "8077158709:AAFDbx2Ek7WhAA6WBiIOWrLuMpAserStCo0"
 ADMIN_IDS = [1236771535]
 LOG_GROUP_ID = -1000000000000  # ← сюда вставь ID своей лог-группы, если нужна отправка логов
 
